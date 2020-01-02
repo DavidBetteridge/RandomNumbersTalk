2a593beae804440a79af30a168ed956e7a40ed03
David Betteridge <DavidaBetteridge@gmail.com>
2020-01-02 20:58:52 +0000

    Add show middle square method

--- ShowMiddleSquareMethod/Form1.Designer.cs
+++ ShowMiddleSquareMethod/Form1.Designer.cs
@@ -28,13 +28,64 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtStartingValue = new System.Windows.Forms.TextBox();
+            this.cmdNext = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(39, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(199, 40);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Seed:";
+            // 
+            // txtStartingValue
+            // 
+            this.txtStartingValue.Location = new System.Drawing.Point(254, 40);
+            this.txtStartingValue.Name = "txtStartingValue";
+            this.txtStartingValue.Size = new System.Drawing.Size(375, 46);
+            this.txtStartingValue.TabIndex = 1;
+            // 
+            // cmdNext
+            // 
+            this.cmdNext.Location = new System.Drawing.Point(656, 37);
+            this.cmdNext.Name = "cmdNext";
+            this.cmdNext.Size = new System.Drawing.Size(280, 49);
+            this.cmdNext.TabIndex = 2;
+            this.cmdNext.Text = "Next Value";
+            this.cmdNext.UseVisualStyleBackColor = true;
+            this.cmdNext.Click += new System.EventHandler(this.cmdNext_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(16F, 40F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(1001, 1385);
+            this.Controls.Add(this.cmdNext);
+            this.Controls.Add(this.txtStartingValue);
+            this.Controls.Add(this.label1);
+            this.Font = new System.Drawing.Font("Segoe UI", 21.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedDialog;
+            this.Margin = new System.Windows.Forms.Padding(8, 9, 8, 9);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+            this.Text = "Middle Square Method";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtStartingValue;
+        private System.Windows.Forms.Button cmdNext;
     }
 }
 
--- ShowMiddleSquareMethod/Form1.cs
+++ ShowMiddleSquareMethod/Form1.cs
@@ -1,20 +1,83 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
 using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 
 namespace ShowMiddleSquareMethod
 {
     public partial class Form1 : Form
     {
+        private long _seed = int.MinValue;
+        private int _n = int.MinValue;
+        private int _y = 150;
+
         public Form1()
         {
             InitializeComponent();
+            txtStartingValue.Text = "42";
+        }
+
+        private void cmdNext_Click(object sender, EventArgs e)
+        {
+            if (_seed == int.MinValue)
+            {
+                _seed = long.Parse(txtStartingValue.Text);
+                _n = txtStartingValue.Text.Length;
+                txtStartingValue.Enabled = false;
+            }
+
+            using (var g = this.CreateGraphics())
+            {
+                var x = 100;
+                x += AddText(g, $"{_seed}", x, _y, Brushes.Black);
+                x += AddText(g, $"2", x, _y - 10, Brushes.Black);
+                x += AddText(g, $" = ", x, _y, Brushes.Black);
+
+                var squared = (_seed * _seed);
+                x += AddText(g, $"{squared}", x, _y, Brushes.Black);
+
+                x += AddText(g, $" = ", x, _y, Brushes.Black);
+
+                var leadingZeros = "".PadLeft(_n * 2 - squared.ToString().Length, '0');
+                var padded = $"{leadingZeros}{squared}";
+                x += AddText(g, $"{leadingZeros}", x, _y, Brushes.Green);
+                x += AddText(g, $"{squared}", x, _y, Brushes.Black);
+
+                x += AddText(g, $" = ", x, _y, Brushes.Black);
+
+
+                var left = padded.Substring(0, _n / 2);
+                var middle = padded.Substring(_n / 2, _n);
+                var right = padded.Substring((_n / 2) + _n);
+                x += AddText(g, $"{left}", x, _y, Brushes.Black);
+                x += AddText(g, $"{middle}", x, _y, Brushes.Green);
+                x += AddText(g, $"{right}", x, _y, Brushes.Black);
+
+                x += AddText(g, $" => ", x, _y, Brushes.Black);
+
+                x += AddText(g, middle, x, _y, Brushes.Black);
+
+
+                _y += 50;
+            }
+
+
+            _seed = Next(_n, _seed);
+        }
+
+        private int AddText(Graphics g, string text, int x, int y, Brush brush)
+        {
+            g.DrawString(text, this.Font, brush, new Point(x, y));
+            var space = g.MeasureString(text, this.Font, new Point(x, y), new StringFormat(StringFormatFlags.MeasureTrailingSpaces));
+            return (int)space.Width - 10;
+        }
+
+        private static long Next(int n, long seed)
+        {
+            var squared = (seed * seed);
+            var padded = squared.ToString().PadLeft(n * 2, '0');
+            var middle = padded.Substring(n / 2, n);
+
+            return long.Parse(middle);
         }
     }
 }
